02b3274113f08c8a692454d3a66be3c52fda2c2b
elcorre <correa755@hotmail.com>
2023-10-26 20:18:05 -0300

    module 768px

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const selectorPolygons = document.getElementsByClassName('st0')
 const logos = document.getElementsByClassName('logos')
 
+const widthResponsive = 768
+
+const marcas = []
 
 for (let i = 0; i < selectorPolygons.length; i++) {
     const polygon = selectorPolygons[i];
 
-    polygon.addEventListener('mouseover', () => {
+    if (polygon.id) {
+        for (let i = 0; i < logos.length; i++) {
+            const logo = logos[i];
+            const logoName = logo.id.split('-')[1]
+            if (polygon.id.includes(logoName)) {
+                marcas.push({
+                    id: polygon.id,
+                    logo: logo.getAttribute('xlink:href')
+                })
+            }
+        }
+    }
+
+    polygon.addEventListener(window.innerWidth > widthResponsive ? 'mouseover' : 'focus', () => {
         for (let i = 0; i < logos.length; i++) {
             const logo = logos[i];
             const logoName = logo.id.split('-')[1]
@@ -15,7 +31,7 @@ for (let i = 0; i < selectorPolygons.length; i++) {
         }
     })
 
-    polygon.addEventListener('mouseout', () => {
+    polygon.addEventListener(window.innerWidth > widthResponsive ? 'mouseout' : 'blur', () => {
         for (let i = 0; i < logos.length; i++) {
             const logo = logos[i];
             const logoName = logo.id.split('-')[1]
@@ -25,4 +41,23 @@ for (let i = 0; i < selectorPolygons.length; i++) {
         }
     })
 
+    polygon.addEventListener(window.innerWidth < widthResponsive && 'click', (e) => {
+
+        const name = e.target.id
+
+        const marca = marcas.find((marca, i) => marca.id == name)
+        // console.log(marca)
+        Toastify({
+            text: marca.id.split(/(?=[A-Z])/).join(' '),
+            avatar: marca.logo,
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            stopOnFocus: true,
+            onClick: function () { },
+        }).showToast();
+    })
+
 }
+
+
